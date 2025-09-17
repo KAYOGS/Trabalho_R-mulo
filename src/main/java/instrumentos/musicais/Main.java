ba23d7a0e9d0b4bb2371492d502badd57b6c0eb1
Seu Nome <seu@email.com>
2025-09-17 10:44:19 -0300

    criação da estrutura do trabalho

--- src/main/java/instrumentos/musicais/Main.java
+++ src/main/java/instrumentos/musicais/Main.java
@@ -2,6 +2,22 @@ package instrumentos.musicais;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        InstrumentosMusicais violao = new Violao();
+        violao.exibirInformacao();
+        violao.tocar();
+        violao.afinar();
+        InstrumentosMusicais violino = new InstrumentosMusicais("Violino", "Madeira") {
+            @Override
+            public void afinar() {
+                System.out.println("O violino esta sendo afinado...");
+            }
+
+            @Override
+            public void tocar() {
+                System.out.println("O violino esta tocando...");
+            }
+        };
+        violao.afinar();
+        violao.tocar();
     }
 }
---
+++ src/main/java/instrumentos/musicais/Violao.java
@@ -0,0 +1,17 @@
+package instrumentos.musicais;
+
+public class Violao extends InstrumentosMusicais {
+    public Violao() {
+        super("Violão", "Cordas");
+    }
+
+    @Override
+    public void tocar() {
+        System.out.println("O violao esta tocando com acordes.");
+    }
+
+    @Override
+    public void afinar() {
+        System.out.println("Afinando as cordas do violao.");
+    }
+}
