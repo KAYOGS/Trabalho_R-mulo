4ef399194b89c8f613c865c3ce94da766f59075f
okanjota <jaimeejunioorr@gmail.com>
2025-09-18 21:33:22 -0300

    Finalizando Main.java

--- src/main/java/instrumentos/musicais/Main.java
+++ src/main/java/instrumentos/musicais/Main.java
@@ -2,6 +2,8 @@ package instrumentos.musicais;
 
 public class Main {
     public static void main(String[] args) {
-      InstrumentoMusical i = new InstrumentoCordas("violao", "Madeira", 1985, 185, 6, "reto");
+      InstrumentoMusical i= new InstrumentoCordas("violao", "Madeira", 1985, 185, 6, "reto");
+    
+      System.out.println(i);
     }
 }
