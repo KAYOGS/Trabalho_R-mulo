261c10b3af065d0f2754531be9c5c26b1ed1bd1f
Seu Nome <seu@email.com>
2025-09-19 22:26:49 -0300

    adaptaçõe

--- src/main/java/instrumentos/musicais/Main.java
+++ src/main/java/instrumentos/musicais/Main.java
@@ -3,7 +3,6 @@ package instrumentos.musicais;
 public class Main {
     public static void main(String[] args) {
       InstrumentoMusical i= new InstrumentoCordas("violao", "Madeira", 1985, 185, 6, "reto");
-    
       System.out.println(i);
     }
 }
